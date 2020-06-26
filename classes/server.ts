0e005a67f22a742ea3d39ab00478f3dc8f925d39
edsvalen <edsvalen@bancolombia.com.co>
2020-06-26 16:13:53 -0500

    patron singleton- manejar una instancia server

--- classes/server.ts
+++ classes/server.ts
@@ -1,17 +1,40 @@
 import express from 'express';
 import { SERVER_PORT } from '../global/environment';
+import socketIO from "socket.io";
+import http from 'http';
 
 export default class Server {
+    private static _instance: Server;
 
     public app: express.Application;
     public port: number;
 
-    constructor() {
+    public io: socketIO.Server;
+    private httpServer: http.Server;
+
+    private constructor() {
         this.app = express();
         this.port = SERVER_PORT;
+
+        this.httpServer = new http.Server(this.app);
+        this.io = socketIO(this.httpServer);
+
+        this.escucharSockets();
     }
 
-    start( callback:VoidFunction ) {
-        this.app.listen(this.port, callback);
+    public static get instance (){
+        return this._instance || (this._instance = new this());
+    }
+
+    start(callback: VoidFunction) {
+        this.httpServer.listen(this.port, callback);
+    }
+
+    private escucharSockets() { //privado porque funciona con la inicialización de la clase
+        console.log('Escuchando conexiones -sockets');
+
+        this.io.on('connection', cliente => {
+            console.log('cliente conectado');
+        });
     }
 }
