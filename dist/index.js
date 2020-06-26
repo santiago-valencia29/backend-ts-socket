7019fb29cfc5173f6ddf57717fed973e0c0e6139
edsvalen <edsvalen@bancolombia.com.co>
2020-06-26 15:25:40 -0500

    configuración basica finalizada

--- dist/index.js
+++ dist/index.js
@@ -6,10 +6,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const server_1 = __importDefault(require("./classes/server"));
 const router_1 = __importDefault(require("./routes/router"));
 const body_parser_1 = __importDefault(require("body-parser"));
+const cors_1 = __importDefault(require("cors"));
 const server = new server_1.default();
 //BodyParser
 server.app.use(body_parser_1.default.urlencoded({ extended: true }));
 server.app.use(body_parser_1.default.json());
+//CORS
+server.app.use(cors_1.default({ origin: true, credentials: true }));
+//Rutas de servicios
 server.app.use('/', router_1.default);
 server.start(() => {
     console.log(`Servidor corriendo en el puerto ${server.port} `);
