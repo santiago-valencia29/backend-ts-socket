3a62eb2352c6da50fd0ccafb4084a2536c13a4a8
edsvalen <edsvalen@bancolombia.com.co>
2020-06-26 15:20:21 -0500

    ejemplos de rutas

--- dist/index.js
+++ dist/index.js
@@ -4,7 +4,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const server_1 = __importDefault(require("./classes/server"));
+const router_1 = __importDefault(require("./routes/router"));
+const body_parser_1 = __importDefault(require("body-parser"));
 const server = new server_1.default();
+//BodyParser
+server.app.use(body_parser_1.default.urlencoded({ extended: true }));
+server.app.use(body_parser_1.default.json());
+server.app.use('/', router_1.default);
 server.start(() => {
     console.log(`Servidor corriendo en el puerto ${server.port} `);
 });
--- index.ts
+++ index.ts
@@ -1,7 +1,14 @@
 import Server from "./classes/server";
-import { SERVER_PORT } from "./global/environment";
+import router from './routes/router';
+import bodyParser from "body-parser";
 
 const server = new Server();
+
+//BodyParser
+server.app.use(bodyParser.urlencoded({ extended: true }));
+server.app.use(bodyParser.json());
+
+server.app.use('/', router);
 
 server.start(() => {
     console.log(`Servidor corriendo en el puerto ${server.port} `)
